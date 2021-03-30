c4836c4b8c9de75d418d8094fc9a89ddab57b35b
hotire <gngh0101@gmail.com>
2021-03-30 22:31:17 +0900

    Simplify application

--- src/main/java/com/github/hotire/spring/cache/Application.java
+++ src/main/java/com/github/hotire/spring/cache/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCacheApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCacheApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
