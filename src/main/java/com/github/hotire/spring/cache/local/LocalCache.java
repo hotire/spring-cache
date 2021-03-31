283ad3794f455f0a4e4e1816f1a965c42e65b9a4
hotire <gngh0101@gmail.com>
2021-04-01 00:57:00 +0900

    Implement LocalCache

--- src/main/java/com/github/hotire/spring/cache/local/LocalCache.java
+++ src/main/java/com/github/hotire/spring/cache/local/LocalCache.java
@@ -6,28 +6,29 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Getter
 @RequiredArgsConstructor
 public class LocalCache {
     private final StringClient stringClient;
     private final LoadingCache<String, String> cache = CacheBuilder.newBuilder()
                                                                    .expireAfterWrite(1, TimeUnit.SECONDS)
                                                                    .build(new CacheLoader<>() {
-                                                                        @Override
-                                                                        public String load(String key) throws Exception {
-                                                                            try {
-                                                                                log.info("load cache");
-                                                                                cache.putAll(stringClient.getAll());
-                                                                                return cache.getUnchecked(key);
-                                                                            } catch (Exception e) {
-                                                                                log.error(e.getMessage(), e);
-                                                                            }
-                                                                            return "empty";
-                                                                        }
-                                                                    });
+                                                                       @Override
+                                                                       public String load(String key) {
+                                                                           log.info("load cache");
+                                                                           cache.putAll(stringClient.getAll());
+                                                                           return get(key);
+                                                                       }
+                                                                   });
+
+    public String get(final String key) {
+        try {
+            return cache.getUnchecked(key);
+        } catch (Exception e) {
+            return "empty";
+        }
+    }
 }
