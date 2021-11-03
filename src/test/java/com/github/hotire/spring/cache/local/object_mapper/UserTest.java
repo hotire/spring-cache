bd2de6e74e3fef3dbca0725ed4707f189d91a092
hotire <gngh0101@gmail.com>
2021-11-03 09:34:31 +0900

    Verify cast in User

--- src/test/java/com/github/hotire/spring/cache/local/object_mapper/UserTest.java
+++ src/test/java/com/github/hotire/spring/cache/local/object_mapper/UserTest.java
@@ -10,13 +10,15 @@ class UserTest {
 
     @Test
     void cast() throws JsonProcessingException {
+        // given
         final User user = new User("hello", 10);
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new ParameterNamesModule());
 
+        // when
         final String json = objectMapper.writeValueAsString(user);
         final User result = objectMapper.readValue(json, User.class);
 
-        System.out.println(result);
+        // no assert
     }
 }
