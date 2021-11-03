2150e3ba08c6484285785e168cb93d65d70200df
hotire <gngh0101@gmail.com>
2021-11-03 09:08:15 +0900

    Verify LocalCacheTest

--- src/test/java/com/github/hotire/spring/cache/local/LocalCacheTest.java
+++ src/test/java/com/github/hotire/spring/cache/local/LocalCacheTest.java
@@ -1,5 +1,6 @@
 package com.github.hotire.spring.cache.local;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
@@ -7,34 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 class LocalCacheTest {
 
+    @DisplayName("development")
     @Test
     void getCache() throws InterruptedException {
         // given
         final StringClient client = new StringClient();
         final LocalCache cache = new LocalCache(client);
 
-        // when
-        log.info(cache.getCache().getIfPresent("d"));
-        try {
-            log.info(cache.getCache().getUnchecked("d"));
-        } catch (Exception e) {
-            log.info(cache.getCache().getIfPresent("d"));
-            log.info(cache.getCache().getIfPresent("d"));
-            log.info(cache.getCache().getIfPresent("d"));
-        }
-        log.info(cache.getCache().getIfPresent("d"));
-        log.info(cache.getCache().getUnchecked("d"));
-        log.info(cache.getCache().getUnchecked("a"));
-        log.info(cache.getCache().getUnchecked("b"));
-        log.info(cache.getCache().getUnchecked("c"));
-
-        Thread.sleep(1100);
-        log.info(cache.getCache().getUnchecked("c"));
-        log.info(cache.getCache().getUnchecked("d"));
-        log.info(cache.getCache().getUnchecked("d"));
-        Thread.sleep(1100);
-        Thread.sleep(1100);
-        log.info(cache.getCache().getUnchecked("d"));
         // no assert
+        log.info("cache : {}", cache);
     }
 }
